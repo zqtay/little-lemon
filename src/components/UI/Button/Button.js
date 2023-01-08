2ef5dd055a262c249ceaf6a24e6be3c98ef95883
zqtay <zqtay96@gmail.com>
2023-01-09 01:22:04 +0800

    Add button component

---
+++ src/components/UI/Button/Button.js
@@ -0,0 +1,33 @@
+import styles from "./Button.module.scss";
+
+const Button = (props) => {
+  let className = styles.secondary;
+  if (props.primary) {
+    className = styles.primary;
+  }
+  else if (props.secondary) {
+    className = styles.secondary;
+  }
+  else {
+    className = styles.default;
+  }
+
+  if (props.wide) {
+    className += styles.wide;
+  }
+
+  let onClick = (!props.href || props.href.length === 0) ?
+    props.onClick :
+    (e) => {
+      e.preventDefault();
+      window.location.href=props.href;
+    };
+
+  return (
+    <button className={`${styles.button} ${className}`} onClick={onClick} type="button">
+      {(typeof(props.children) === "string") ? props.children : props.text}
+    </button>
+  );
+}
+
+export default Button;
