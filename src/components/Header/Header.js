aaa23b50adfd62b008b851f2603de9852500c845
zqtay <zqtay96@gmail.com>
2023-01-15 17:54:22 +0800

    Use react router

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,14 +2,15 @@ import styles from './Header.module.scss';
 import Nav from '../Nav/Nav';
 import Container from "../UI/Container/Container";
 import { ROOT_URL } from "../../Util";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <header>
       <Container className={styles.container}>
-        <a href="/">
+        <Link to="/">
           <img className={styles.logo} alt="logo" src={`${ROOT_URL}/logo.png`}></img>
-        </a>
+        </Link>
         <div className={styles["nav-container"]}>
           <button className={styles["nav-button"]}>{">"}</button>
           <Nav className={styles["nav-header"]}></Nav>
